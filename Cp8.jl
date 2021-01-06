896b8be50543c49a58f5ca6220c3dc3ef68fbcb8
tkawano-kyoto <t.k.kyoto@gmail.com>
2021-01-06 16:49:52 +0900

    Chapter Exersise 8-11追加

--- Cp8.jl
+++ Cp8.jl
@@ -23,3 +23,80 @@ function my_count(word::String,letter)
     end
     return counter
 end
+
+# Chapter8 Exercise 8-6
+
+function isreverse(word1,word2)
+    if length(word1) != length(word2)
+        return false
+    end
+    i=firstindex(word1)
+    j=lastindex(word2)
+
+    while  i <= length(word1)
+        if word1[i] != word2[j]
+            return false
+        end
+        i=nextind(word1,i)
+        j=prevind(word2,j)
+    end
+    true
+end
+
+# Chapter8 Exercise 8-8
+
+count(isequal('a'),"banana")
+
+
+# Chapter8 Exercise 8-11 
+
+
+function caecer_cipher(str,shift)
+	
+	alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+	decipher = ""
+	upper_str = uppercase(str)
+	
+	i = firstindex(upper_str)
+	
+	while i <= length(upper_str)
+		j = findfirst(upper_str[i],alphabet)
+		j -= shift
+		if j < 1
+			j = 26 + j
+		end
+		decipher *= alphabet[j]
+		i = nextind(upper_str,i)
+	end
+	return decipher
+	
+end
+
+# test = "WATASINOMAMAEWANAKANODESU" 
+# caecer_cipher(test,3)
+# dicipher =  "ZDWDVLQRQDPDHZDQDNDQRGHVXABC"
+
+
+function caecer_dicipher(str,shift)
+	
+	alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+	cipher = ""
+	upper_str = uppercase(str)
+	
+	i = firstindex(upper_str)
+	
+	while i <= length(upper_str)
+		j = findfirst(upper_str[i],alphabet)
+		j += shift
+		if j > 26
+			j = j - 26
+		end
+		cipher *= alphabet[j]
+		i = nextind(upper_str,i)
+	end
+	return cipher
+end
+
+# test = "ZDWDVLQRQDPDHZDQDNDQRGHVXABC"
+# caecer_dicipher(test,3)
+# cipher = "WATASINOMAMAEWANAKANODESU"
